f3f8b6c6695b246d6cc4cc143d1738de7b72d018
Danil Kharitsky <khlive.ftf@gmail.com>
2022-07-17 23:05:54 +0500

    feat(project): более осмысленное приложение

---
+++ src/service_one/tests/test_items.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+from main import app
+from routers.items import fake_items_db
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/items/", headers={"X-Token": "fake-super-secret-token"}, params={"token": "jessica"})
+
+    assert response.status_code == 200
+    assert response.json() == fake_items_db
--- src/tests/test_fx.py
+++
@@ -1,5 +0,0 @@
-from main import f
-
-def test_fx():
-    x = 3
-    assert 9 == f(x)
